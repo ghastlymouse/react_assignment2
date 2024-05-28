087ba3bf8ad7b80f8c276b43e2831ae9537a226e
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-05-28 12:38:25 +0900

    useEffect로 조건부 스타일링

--- src/components/MonthSelect.jsx
+++ src/components/MonthSelect.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components'
 import { changeMonth } from "../redux/slices/listMonth";
 
 const MonthSelect = () => {
+    const { selectedMonth } = useSelector(state => state.listMonth);
     const dispatch = useDispatch();
 
     const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -16,11 +17,14 @@ const MonthSelect = () => {
         }
     }, []);
 
+    useEffect(() => {
+        setActiveMonth(selectedMonth);
+    }, [selectedMonth])
+
     const [activeMonth, setActiveMonth] = useState(loadedLastSelectMonth || 1);
 
     const handleSelectMonth = (month) => {
         localStorage.setItem("lastSelect", month);
-        setActiveMonth(month);
         dispatch(changeMonth(month));
     };
 
