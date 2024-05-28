48031e4694d37845db6d3016dc6c9c1c6164e0a5
무시무시한쥐 <163982251+ghastlymouse@users.noreply.github.com>
2024-05-28 15:05:40 +0900

    모달 추가

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +7,10 @@ import { changeMonth } from '../redux/slices/listMonth';
 
 const AddExpense = () => {
     const dispatch = useDispatch();
+
+    const [openModal, setOpenModal] = useState(false);
+    const [alertMonth, setAlertMonth] = useState(1);
+    const modalBg = useRef();
 
     const handleSubmitForm = (event) => {
         event.preventDefault();
@@ -29,42 +33,57 @@ const AddExpense = () => {
             description,
         }));
         const selectedMonth = +date.slice(5, 7);
-        alert(`${selectedMonth}월의 지출 내역에 추가되었습니다!`);
         dispatch(changeMonth(selectedMonth));
+        setAlertMonth(selectedMonth);
+        setOpenModal(true);
     }
 
     const thisYearFirstDay = `${new Date().getFullYear()}-01-01`;
     const thisYearLastDay = `${new Date().getFullYear()}-12-31`;
 
     return (
-        <StForm onSubmit={handleSubmitForm}>
-            <label htmlFor='date'>날짜</label>
-            <StInput
-                name="date"
-                type="date"
-                min={thisYearFirstDay}
-                max={thisYearLastDay}
-                required />
-            <label htmlFor='item'>항목</label>
-            <StInput
-                name="item"
-                type="text"
-                placeholder='지출 항목'
-                required />
-            <label htmlFor='amount'>금액</label>
-            <StInput
-                name="amount"
-                type="number"
-                placeholder='지출 금액'
-                required />
-            <label htmlFor='description'>내용</label>
-            <StInput
-                name="description"
-                type="text"
-                placeholder='지출 내용'
-                required />
-            <StSubmitBtn type="submit">추가</StSubmitBtn>
-        </StForm>
+        <>
+            <ModalBackground $openModal={openModal} ref={modalBg} onClick={(e) => {
+                if (e.target === modalBg.current) {
+                    setOpenModal(false);
+                }
+            }}>
+                <Modal>
+                    <span>{alertMonth}월의 지출 내역에 추가되었습니다!</span>
+                    <ModalBtnDiv>
+                        <ModalBtn onClick={() => setOpenModal(false)}>확인</ModalBtn>
+                    </ModalBtnDiv>
+                </Modal>
+            </ModalBackground>
+            <StForm onSubmit={handleSubmitForm}>
+                <label htmlFor='date'>날짜</label>
+                <StInput
+                    name="date"
+                    type="date"
+                    min={thisYearFirstDay}
+                    max={thisYearLastDay}
+                    required />
+                <label htmlFor='item'>항목</label>
+                <StInput
+                    name="item"
+                    type="text"
+                    placeholder='지출 항목'
+                    required />
+                <label htmlFor='amount'>금액</label>
+                <StInput
+                    name="amount"
+                    type="number"
+                    placeholder='지출 금액'
+                    required />
+                <label htmlFor='description'>내용</label>
+                <StInput
+                    name="description"
+                    type="text"
+                    placeholder='지출 내용'
+                    required />
+                <StSubmitBtn type="submit">추가</StSubmitBtn>
+            </StForm>
+        </>
     )
 }
 
@@ -108,4 +127,50 @@ const StInput = styled.input`
     border-radius: 10px;
     font-family: inherit;
     font-size: inherit;
+`;
+
+const ModalBackground = styled.div`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: ${props => props.$openModal ? "flex" : "none"};
+    justify-content: center;
+    background: rgba(0, 0, 0, 0.4);
+`;
+
+const Modal = styled.div`
+    width: 500px;
+    height: 150px;
+    background-color: white;
+    border: 3px solid black;
+    border-radius: 10px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const ModalBtnDiv = styled.div`
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+`;
+
+const ModalBtn = styled.button`
+    border: none;
+    border-radius: 4px;
+    background-color: blue;
+    padding: 10px 20px;
+    width: 100%;
+    height: 35px;
+    color: white;
+    font-family: inherit;
+    font-size: 15px;
+    cursor: pointer;
+    &:hover{
+        filter:brightness(0.8);
+    }
 `;
